5966d8d09ebc20b4b3e014e867f1dc26a9ac6b12
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2025-02-26 13:51:32 +0530

    OOPS concepts added

--- Perceptron Network/main.py
+++ Perceptron Network/main.py
@@ -1,41 +1,52 @@
 from utility_functions import *
 
-inputs = [
-    [1,1,1,1],
-    [-1,1,-1,-1],
-    [1,1,1,-1],
-    [1,-1,-1,1]
-]
-bias = 0
-weights = [0,0,0,0,bias]
-targets = [1,1,-1,-1]
-alpha = 1
+class perceptron():
+    inputs, weights, bias, output, targets, net_input, alpha = None, None, None, None, None, None, None
+    def __init__(self):
+        self.inputs = [
+            [1,1,1,1],
+            [-1,1,-1,-1],
+            [1,1,1,-1],
+            [1,-1,-1,1]
+        ]
+        self.bias = 0
+        self.weights = [0,0,0,0,self.bias]
+        self.targets = [1,1,-1,-1]
+        self.alpha = 1
 
-net_input = []
-epoch = 0
+        self.net_input = []
+        self.epoch = 0
 
-while(True):
-    initial_weights = weights
-    epoch += 1
-    print('EPOCH '+ str(epoch))
-    for i in range(len(inputs)):
-        net_input = calculate_net_input(inputs[i], weights, bias)
-        output = activation_function(net_input)
-        weight_change = calculate_weight_change(inputs[i], targets[i], alpha)
-        if(targets[i] != output):
-            weights = update_weights(weights,weight_change)
-        bias = weights[-1]
+    def training(self):
+        while(True):
+            initial_weights = self.weights
+            self.epoch += 1
+            print('EPOCH '+ str(self.epoch))
+            for i in range(len(self.inputs)):
+                self.net_input = calculate_net_input(self.inputs[i], self.weights, self.bias)
+                self.output = activation_function(self.net_input)
+                self.weight_change = calculate_weight_change(self.inputs[i], self.targets[i], self.alpha)
+                if(self.targets[i] != self.output):
+                    self.weights = update_weights(self.weights, self.weight_change)
+                self.bias = self.weights[-1]
 
-        print("input: ", inputs[i])
-        print("Target: ",targets[i])
-        print("Net Input ", net_input)
-        print("output: ", output)
-        print("Weight Change: ",weight_change)
-        print("Weights: ",weights)
-        print("\n\n\n")
+                print("input: ", self.inputs[i])
+                print("Target: ",self.targets[i])
+                print("Net Input ", self.net_input)
+                print("output: ", self.output)
+                print("Weight Change: ",self.weight_change)
+                print("Weights: ", self.weights)
+                print("\n\n\n")
 
-    if(initial_weights == weights):
-        break
+            if(initial_weights == self.weights):
+                break
+
+def main():
+    obj  = perceptron()
+    obj.training()
+
+if __name__ == "__main__":
+    main()
         
 
 
